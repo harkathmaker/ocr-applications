3fc190d52a06f229400f4c0ca6700b9dfb9cf26a
kp <kp70726F@gmail.com>
2014-03-09 14:15:32 -0700

    Added timings to simple_stream.c

--- stream/simple_stream.c
+++ stream/simple_stream.c
@@ -4,6 +4,7 @@
 /* STREAM Headers */
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h> 
 
 #ifndef STREAM_ARRAY_SIZE
 #	define STREAM_ARRAY_SIZE	100
@@ -17,29 +18,40 @@
 #	define STREAM_TYPE double
 #endif
 
+double mysecond() {
+	struct timeval tp;
+	struct timezone tzp;
+	int i = gettimeofday(&tp, &tzp);
+	return ((double) tp.tv_sec + (double) tp.tv_usec * 1.e-6 );
+}
+
 ocrGuid_t iterEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	STREAM_TYPE * data = (STREAM_TYPE *) depv[0].ptr;
 	u64 i;
+	STREAM_TYPE scalar = 3.0;
+
+	data[3 * STREAM_ARRAY_SIZE + paramv[0] - 1] = mysecond();
 
 	for (i = 0; i < STREAM_ARRAY_SIZE; i++)
 		data[2 * STREAM_ARRAY_SIZE + i] = data[i];
 	// PRINTF("FINISHED COPY\n");
-	
-	STREAM_TYPE scalar = 3.0;
+
 	for (i = 0; i < STREAM_ARRAY_SIZE; i++)
 		data[STREAM_ARRAY_SIZE + i] = scalar * data[2 * STREAM_ARRAY_SIZE + i];
 	// PRINTF("FINISHED SCALE\n");
-	
+
 	for (i = 0; i < STREAM_ARRAY_SIZE; i++)
 		data[2 * STREAM_ARRAY_SIZE + i] = data[i] + data[STREAM_ARRAY_SIZE + i];
 	// PRINTF("FINISHED ADD\n");
-	
+
 	for (i = 0; i < STREAM_ARRAY_SIZE; i++)
 		data[i] = data[STREAM_ARRAY_SIZE + i] + scalar * data[2 * STREAM_ARRAY_SIZE + i];
 	// PRINTF("FINISHED TRIAD\n");
-	
+
+	data[3 * STREAM_ARRAY_SIZE + paramv[0] - 1] = mysecond() - data[3 * STREAM_ARRAY_SIZE + paramv[0] - 1];
+
 	// PRINTF("FINISHED ITER\n");
-	
+
 	if (paramv[0] < paramv[1]) {
 		ocrGuid_t dataGuid = (ocrGuid_t) depv[0].guid;
 		paramv[0] += 1;
@@ -58,19 +70,20 @@ ocrGuid_t iterEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	return NULL_GUID;
 }
 
-ocrGuid_t quickCheckEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
+ocrGuid_t resultsEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	u64 i;
 	STREAM_TYPE * data = (STREAM_TYPE *) depv[1].ptr;
 	STREAM_TYPE a = data[0];
 	STREAM_TYPE b = data[STREAM_ARRAY_SIZE];
 	STREAM_TYPE c = data[2 * STREAM_ARRAY_SIZE];
 	STREAM_TYPE ai, bi, ci, scalar;
-	
+	STREAM_TYPE timingsum = 0.0;
+
 	// Reproduce initializations
 	ai = 1.0;
 	bi = 2.0;
 	ci = 0.0;
-	
+
 	// Execute timing loop
 	scalar = 3.0;
 	for (i = 0; i < NTIMES; i++) {
@@ -79,7 +92,15 @@ ocrGuid_t quickCheckEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) 
 		ci = ai + bi;
 		ai = bi + scalar * ci;
 	}
-	
+
+	PRINTF("Timing Results:\n");
+	for (i = 0; i < NTIMES; i++) {
+		STREAM_TYPE timing = data[3 * STREAM_ARRAY_SIZE + i];
+		PRINTF("TRIAL %d: %f s\n", i + 1, timing);
+		timingsum += timing;
+	}
+	PRINTF("AVERAGE: %f s\n", timingsum / NTIMES);
+
 	PRINTF("After %d Iterations:\n", NTIMES);
 	if ((ai - a + bi - b + ci - c) == 0)
 		PRINTF("No differences between expected and actual\n");
@@ -97,13 +118,15 @@ ocrGuid_t mainEdt(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
 	// Preparing datablock
 	STREAM_TYPE * dataArray;
 	ocrGuid_t dataGuid;
-	DBCREATE(&dataGuid,(void **) &dataArray, sizeof(ocrGuid_t)*(3 * STREAM_ARRAY_SIZE), 0, NULL_GUID, NO_ALLOC);
+	DBCREATE(&dataGuid,(void **) &dataArray, sizeof(ocrGuid_t)*(3 * STREAM_ARRAY_SIZE + NTIMES), 0, NULL_GUID, NO_ALLOC);
 	for (i = 0; i < STREAM_ARRAY_SIZE; i++){
 		dataArray[i] = 1.0;
 		dataArray[STREAM_ARRAY_SIZE + i] = 2.0;
 		dataArray[2 * STREAM_ARRAY_SIZE + i] = 0.0;
 	}
-	
+	for (i = 3 * STREAM_ARRAY_SIZE; i < NTIMES; i++)
+		dataArray[i] = 0.0;
+
 	// EDT Template for iterator
 	ocrGuid_t iterTemplateGuid;
 	ocrEdtTemplateCreate(&iterTemplateGuid, iterEdt, sizeof(nparamv), 1);
@@ -116,18 +139,18 @@ ocrGuid_t mainEdt(u32 paramc, u64* paramv, u32 depc, ocrEdtDep_t depv[]) {
 	// Dependencies for iterator
 	ocrAddDependence(dataGuid, iterGuid, 0, DB_MODE_EW);
 
-	// EDT Template for quickCheck
-	ocrGuid_t quickCheckTemplateGuid;
-	ocrEdtTemplateCreate(&quickCheckTemplateGuid, quickCheckEdt, 0, 2);
+	// EDT Template for results
+	ocrGuid_t resultsTemplateGuid;
+	ocrEdtTemplateCreate(&resultsTemplateGuid, resultsEdt, 0, 2);
 
-	// EDT for quickCheck
-	ocrGuid_t quickCheckGuid;
-	ocrEdtCreate(&quickCheckGuid, quickCheckTemplateGuid, EDT_PARAM_DEF, NULL_GUID, EDT_PARAM_DEF, NULL_GUID,
+	// EDT for results
+	ocrGuid_t resultsGuid;
+	ocrEdtCreate(&resultsGuid, resultsTemplateGuid, EDT_PARAM_DEF, NULL_GUID, EDT_PARAM_DEF, NULL_GUID,
 		EDT_PROP_NONE, NULL_GUID, NULL);
-	
-	// Dependencies for quickCheck
-	ocrAddDependence(iterOutput, quickCheckGuid, 0, DB_MODE_RO);
-	ocrAddDependence(dataGuid, quickCheckGuid, 1, DB_MODE_RO);
+
+	// Dependencies for results
+	ocrAddDependence(iterOutput, resultsGuid, 0, DB_MODE_RO);
+	ocrAddDependence(dataGuid, resultsGuid, 1, DB_MODE_RO);
 
 	// PRINTF("FINISHED MAIN\n");
 	return NULL_GUID;
