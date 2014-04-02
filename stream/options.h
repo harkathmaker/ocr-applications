2a9b6c7e460f41aeeab08f813224f4adde9975f2
kp <kp70726F@gmail.com>
2014-04-02 00:49:21 -0700

    Added getopt support for parallel_stream.c and updated options.h as well as README

--- stream/options.h
+++ stream/options.h
@@ -15,17 +15,19 @@ void printHelp() {
 			"serial_stream [-d db_size] [-e file_name] [-h] [-i num_iter]\n"
 			"              [-r] [-s scalar_value] [-v]\n\n"
 			"List of Options:\n" 
-			"-d|--db_size     = Size of data blocks a, b, c (1 million by default).\n"
-			"-e|--export      = Exports results to csv file.\n"
-			"-h|--help        = List of options.\n"
-			"-i|--iterations  = Number of iterations (1 by default).\n"
-			"-r|--verify      = Verify results.\n"
-			"-s|--scalar      = Set scalar value (3.0 by default).\n"
-			"-v|--verbose     = Verbose output.\n");
+			"  -d|--db_size     = Size of data blocks a, b, c (1 million by default).\n"
+			"  -e|--export      = Exports results to csv file.\n"
+			"  -h|--help        = List of options.\n"
+			"  -i|--iterations  = Number of iterations (1 by default).\n"
+			"* -p|--split       = Splits db_size by given value (split value should be set so db_size % split = 0 and is 1 by default).\n"
+			"  -r|--verify      = Verify results.\n"
+			"  -s|--scalar      = Set scalar value (3.0 by default).\n"
+			"  -v|--verbose     = Verbose output.\n"
+			"Options with a * next to them are only available in parallel implementations\n");
 	return;
 }
 
-int parseOptions(int argc, char ** argv, u64 * db_size, char * efile, u64 * iterations, STREAM_TYPE * scalar, int * verify, int * verbose) {
+int parseOptions(int argc, char ** argv, u64 * db_size, char * efile, u64 * iterations, int * verify, STREAM_TYPE * scalar, int * verbose) {
 	char c;
 	int help = 0;
 	efile = NULL;
@@ -42,13 +44,13 @@ int parseOptions(int argc, char ** argv, u64 * db_size, char * efile, u64 * iter
 			{"export_to",   required_argument,  0,              'e'},
 			{"help",        no_argument,        0,              'h'},
 			{"iterations",  required_argument,  0,              'i'},
-			{"scalar",      required_argument,  0,              's'},
 			{"verify",      no_argument,        0,              'r'},
+			{"scalar",      required_argument,  0,              's'},
 			{"verbose",     no_argument,        0,              'v'},
 			{0, 0, 0, 0}
 		};
 
-		c = getopt_long(argc, argv, "e:hi:n:rs:v", long_options, &option_index);
+		c = getopt_long(argc, argv, "d:e:hi:rs:v", long_options, &option_index);
 		if (c == -1)
 			break;
 		switch (c) {
@@ -63,7 +65,7 @@ int parseOptions(int argc, char ** argv, u64 * db_size, char * efile, u64 * iter
 				help = 1;
 				break;
 			case 'i':
-				sscanf(optarg, "%d", iterations);
+				sscanf(optarg, "%llu", iterations);
 				break;
 			case 'r':
 				*verify = 1;
@@ -79,5 +81,68 @@ int parseOptions(int argc, char ** argv, u64 * db_size, char * efile, u64 * iter
 				break;
 		}
 	}
+	return help;
+}
+
+int parseParallelOptions(int argc, char ** argv, u64 * db_size, char * efile, u64 * iterations, u64 * split, u64 * chunk, int * verify, STREAM_TYPE * scalar, int * verbose) {
+	char c;
+	int help = 0;
+	efile = NULL;
+	*iterations = 1;
+	*db_size = 1000000;
+	*split = 1;
+	*scalar = 3.0;
+	*verify = 0;
+	*verbose = 0;
+
+	while (1) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"db_size",     required_argument,  0,              'd'},
+			{"export_to",   required_argument,  0,              'e'},
+			{"help",        no_argument,        0,              'h'},
+			{"iterations",  required_argument,  0,              'i'},
+			{"split",       required_argument,  0,              'p'},
+			{"verify",      no_argument,        0,              'r'},
+			{"scalar",      required_argument,  0,              's'},
+			{"verbose",     no_argument,        0,              'v'},
+			{0, 0, 0, 0}
+		};
+
+		c = getopt_long(argc, argv, "d:e:hi:n:rs:v", long_options, &option_index);
+		if (c == -1)
+			break;
+		switch (c) {
+			case 'd':
+				sscanf(optarg, "%llu", db_size);
+				break;
+			case 'e':
+				sscanf(optarg, "%s", efile);
+				break;
+			case 'h':
+				printHelp();
+				help = 1;
+				break;
+			case 'i':
+				sscanf(optarg, "%llu", iterations);
+				break;
+			case 'p':
+				sscanf(optarg, "%llu", split);
+				break;
+			case 'r':
+				*verify = 1;
+				break;
+			case 's':
+				sscanf(optarg, "%lf", scalar);
+				break;
+			case 'v':
+				*verbose = 1;
+				break;
+			case '?':
+				PRINTF("Unknown option -%c.\n",optopt);
+				break;
+		}
+	}
+	*chunk = *db_size / *split;
 	return help;
 }
--- stream/parallel_stream.c
+++ stream/parallel_stream.c
@@ -1,40 +1,24 @@
-#define __OCR__
-#include "ocr.h"
+#include "options.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/time.h> 
+
+#ifndef __OCR__
+#	define __OCR__
+#	include "ocr.h"
+#endif
 
 #ifndef STREAM_TYPE
 #	define STREAM_TYPE double
 #endif
 
-#ifndef STREAM_ARRAY_SIZE
-#	define STREAM_ARRAY_SIZE	100
-#endif
-
-#ifndef NSPLIT
-#	define NSPLIT	10
-#endif
-
-#ifndef CHUNK
-#	define CHUNK	STREAM_ARRAY_SIZE / NSPLIT
-#endif
-
-#ifndef NTIMES
-#	define NTIMES	5
-#endif
-
-#ifndef SCALAR
-#	define SCALAR	3.0
-#endif
-
 /*
 	NOTES: 
 	One large datablock is used with ranges of indexes corresponding to the following:
-			  a --> [0 to (STREAM_ARRAY_SIZE - 1)]
-			  b --> [STREAM_ARRAY_SIZE to (2 * STREAM_ARRAY_SIZE - 1)]
-			  c --> [2 * STREAM_ARRAY_SIZE to (3 * STREAM_ARRAY_SIZE - 1)]
-		timings --> [3 * STREAM_ARRAY_SIZE to (3 * STREAM_ARRAY_SIZE + NTIMES - 1)]
+			  a --> [0 to (db_size - 1)]
+			  b --> [db_size to (2 * db_size - 1)]
+			  c --> [2 * db_size to (3 * db_size - 1)]
+		timings --> [3 * db_size to (3 * db_size + iterations - 1)]
 */
 
 double mysecond() {
@@ -46,47 +30,53 @@ double mysecond() {
 
 ocrGuid_t pipelineEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	u64 i;
+	u64 db_size = paramv[1];
 	u64 begin = paramv[paramc - 2];
 	u64 end = paramv[paramc - 1];
+	STREAM_TYPE scalar = (STREAM_TYPE) paramv[5];
 	STREAM_TYPE * data = (STREAM_TYPE *) depv[0].ptr;
 
-	data[3 * STREAM_ARRAY_SIZE + paramv[0] - 1] = mysecond();
+	data[3 * db_size + paramv[0] - 1] = mysecond();
 
 	for (i = begin; i < end; i++)
-		data[2 * STREAM_ARRAY_SIZE + i] = data[i];
+		data[2 * db_size + i] = data[i];
 	// PRINTF("FINISHED COPY\n");
 
 	for (i = begin; i < end; i++)
-		data[STREAM_ARRAY_SIZE + i] = SCALAR * data[2 * STREAM_ARRAY_SIZE + i];
+		data[db_size + i] = scalar * data[2 * db_size + i];
 	// PRINTF("FINISHED SCALE\n");
 
 	for (i = begin; i < end; i++)
-		data[2 * STREAM_ARRAY_SIZE + i] = data[i] + data[STREAM_ARRAY_SIZE + i];
+		data[2 * db_size + i] = data[i] + data[db_size + i];
 	// PRINTF("FINISHED ADD\n");
 
 	for (i = begin; i < end; i++)
-		data[i] = data[STREAM_ARRAY_SIZE + i] + SCALAR * data[2 * STREAM_ARRAY_SIZE + i];
+		data[i] = data[db_size + i] + scalar * data[2 * db_size + i];
 	// PRINTF("FINISHED TRIAD\n");
 
-	data[3 * STREAM_ARRAY_SIZE + paramv[0] - 1] = mysecond() - data[3 * STREAM_ARRAY_SIZE + paramv[0] - 1];
+	data[3 * db_size + paramv[0] - 1] = mysecond() - data[3 * db_size + paramv[0] - 1];
 
 	// PRINTF("FINISHED PIPELINE\n");
 	return NULL_GUID;
 }
 
+//                   0  1        2           3      4      5       6                     7                     8
+// u64 nparamv[9] = {1, db_size, iterations, split, chunk, scalar, pipeExecTemplateGuid, nextIterTemplateGuid, pipelineTemplateGuid};
 ocrGuid_t pipeExecEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
-	u64 i, nparamv[6] = {0};
+	u64 i, nparamv[11] = {0};
+	u64 split = paramv[3];
+	u64 chunk = paramv[4];
 	ocrGuid_t dataGuid = (ocrGuid_t) depv[0].guid;
-	ocrGuid_t pipelineTemplateGuid = paramv[3];
+	ocrGuid_t pipelineTemplateGuid = paramv[8];
 	ocrGuid_t pipelineGuid;
 
 	for (i = 0; i < paramc; i++)
 		nparamv[i] = paramv[i];
 
-	// Spawn pipeline children operating on CHUNK amounts of data
-	for (i = 0; i < NSPLIT; i++) {
-		nparamv[paramc] = i * CHUNK;
-		nparamv[paramc + 1] = (i + 1) * CHUNK;
+	// Spawn pipeline children operating on chunk amounts of data
+	for (i = 0; i < split; i++) {
+		nparamv[paramc] = i * chunk;
+		nparamv[paramc + 1] = (i + 1) * chunk;
 		ocrEdtCreate(&pipelineGuid, pipelineTemplateGuid, EDT_PARAM_DEF, nparamv, EDT_PARAM_DEF, NULL_GUID,
 				 EDT_PROP_FINISH, NULL_GUID, NULL);
 		ocrAddDependence(dataGuid, pipelineGuid, 0, DB_MODE_ITW);
@@ -94,114 +84,150 @@ ocrGuid_t pipeExecEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	return NULL_GUID;
 }
 
+// u64 nparamv[6] = {1, db_size, iterations, split, chunk, scalar, pipeExecTemplateGuid, nextIterTemplateGuid, pipelineTemplateGuid};
 ocrGuid_t iterEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	ocrGuid_t dataGuid = (ocrGuid_t) depv[0].guid;
-	ocrGuid_t pipeExecTemplateGuid = paramv[1];
-	ocrGuid_t nextIterTemplateGuid = paramv[2];
+	u64 iterations = paramv[2];
+	ocrGuid_t pipeExecTemplateGuid = paramv[6];
+	ocrGuid_t nextIterTemplateGuid = paramv[7];
 	ocrGuid_t pipeExecGuid, pipeExecDone, nextIterGuid;
 
 	ocrEdtCreate(&pipeExecGuid, pipeExecTemplateGuid, EDT_PARAM_DEF, paramv, EDT_PARAM_DEF, NULL_GUID,
 				 EDT_PROP_FINISH, NULL_GUID, &pipeExecDone);
 
 	// Setup next iteration
-	if (paramv[0] < NTIMES) {
+	if (paramv[0] < iterations) {
 		paramv[0] += 1;
 		ocrEdtCreate(&nextIterGuid, nextIterTemplateGuid, EDT_PARAM_DEF, paramv, EDT_PARAM_DEF, NULL_GUID,
 					 EDT_PROP_FINISH, NULL_GUID, NULL);
-		ocrAddDependence(dataGuid, nextIterGuid, 0, DB_MODE_EW);
+		ocrAddDependence(dataGuid, nextIterGuid, 0, DB_MODE_ITW);
 		ocrAddDependence(pipeExecDone, nextIterGuid, 1, DB_MODE_RO);
 	} 
 
 	// Dependencies for pipeline
-	ocrAddDependence(dataGuid, pipeExecGuid, 0, DB_MODE_EW);
+	ocrAddDependence(dataGuid, pipeExecGuid, 0, DB_MODE_ITW);
+	// PRINTF("FINISHED ITER\n");
 	return NULL_GUID;
 }
 
+//                   0        1           2      3      4       5       6
+// u64 rparamv[7] = {db_size, iterations, split, chunk, verify, scalar, verbose};
 ocrGuid_t resultsEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
 	u64 i, j;
+	u64 db_size = paramv[0];
+	u64 iterations = paramv[1];
+	u64 split = paramv[2];
+	int verify = (int) paramv[4];
+	int verbose = (int) paramv[6];
 	STREAM_TYPE * data = (STREAM_TYPE *) depv[0].ptr;
-	STREAM_TYPE a, ai, b, bi, c, ci;
-	STREAM_TYPE totalsum, timingsum[NTIMES];
-	a = b = c = 0;
+	STREAM_TYPE totalsum, timingsum[iterations];
 
-	// Reproduce initializations
-	ai = 1.0;
-	bi = 2.0;
-	ci = 0.0;
-
-	// Execute timing loop
-	for (i = 0; i < NTIMES; i++) {
-		ci = ai;
-		bi = SCALAR * ci;
-		ci = ai + bi;
-		ai = bi + SCALAR * ci;
-	}
-
-	for (i = 0; i < NSPLIT; i++) {
-		a += data[i];
-		b += data[STREAM_ARRAY_SIZE + i];
-		c += data[2 * STREAM_ARRAY_SIZE + i];
-		for (j = 0; j < NTIMES; j++) {
-			timingsum[j] += data[3 * STREAM_ARRAY_SIZE + j];
-		}
-	}
+	for (i = 0; i < split; i++)
+		for (j = 0; j < iterations; j++)
+			timingsum[j] += data[3 * db_size + j];
 
 	PRINTF("Timing Results:\n");
-	for (i = 0; i < NTIMES; i++) {
+	for (i = 0; i < iterations; i++) {
 		PRINTF("TRIAL %d: %f s\n", i + 1, timingsum[i]);
 		totalsum += timingsum[i];
 	}
-	PRINTF("AVERAGE Time Per CHUNK: %f s\n", totalsum / NTIMES / NSPLIT);
-	PRINTF("AVERAGE Time Per Trial: %f s\n", totalsum / NTIMES);
+	PRINTF("AVERAGE Time Per chunk: %f s\n", totalsum / iterations / split);
+	PRINTF("AVERAGE Time Per Trial: %f s\n", totalsum / iterations);
 
-	PRINTF("After %d Iterations:\n", NTIMES);
-	if ((NSPLIT * ai - a + NSPLIT * bi - b + NSPLIT * ci - c) == 0)
-		PRINTF("No differences between expected and actual\n");
-	else  
-		PRINTF("Expected a: %f, Actual a: %f\n"
-			   "Expected b: %f, Actual b: %f\n"
-			   "Expected c: %f, Actual c: %f\n"
-			   "Note: Expected values are multiplied by NSPLIT (i.e. a * NSPLIT)\n"
-				"     Actual values are sum of NSPLIT number of first elements", 
-				NSPLIT * ai, a, NSPLIT * bi, b, NSPLIT * ci, c);
+	if (verify) {
+		STREAM_TYPE a = 0, ai, b = 0, bi, c = 0, ci;
+		STREAM_TYPE scalar = (STREAM_TYPE) paramv[5];
 
+		// Sum actual values
+		for (i = 0; i < split; i++) {
+			a += data[i];
+			b += data[db_size + i];
+			c += data[2 * db_size + i];
+		}
+
+		// Reproduce initializations
+		ai = 1.0;
+		bi = 2.0;
+		ci = 0.0;
+
+		// Execute timing loop
+		for (i = 0; i < iterations; i++) {
+			ci = ai;
+			bi = scalar * ci;
+			ci = ai + bi;
+			ai = bi + scalar * ci;
+		}
+
+		PRINTF("After %d Iterations:\n", iterations);
+		if ((split * ai - a + split * bi - b + split * ci - c) == 0)
+			PRINTF("No differences between expected and actual\n");
+		else  
+			PRINTF("Expected a: %f, Actual a: %f\n"
+				   "Expected b: %f, Actual b: %f\n"
+				   "Expected c: %f, Actual c: %f\n"
+				   "Note: Expected values are multiplied by split (i.e. a * split)\n"
+					"     Actual values are sum of split number of first elements", 
+					split * ai, a, split * bi, b, split * ci, c);
+	}
 	ocrShutdown();
 	return NULL_GUID;
 }
 
 ocrGuid_t mainEdt(u32 paramc, u64 * paramv, u32 depc, ocrEdtDep_t depv[]) {
-	u64 i, nparamc = 4, pparamc = 2;
+	u64 c, i, argc = getArgc(depv[0].ptr);
+	char * argv[argc];
+	for(i = 0; i < argc; i++)
+		argv[i] = getArgv(depv[0].ptr, i);
+
+	// Getopt arguments
+	u64 db_size;
+	char efile[100];
+	u64 iterations;
+	u64 split;
+	u64 chunk;
+	int verify;
+	STREAM_TYPE scalar;
+	int verbose;
+
+	// Parse getopt commands, shutdown and exit if help is selected
+	if (parseParallelOptions(argc, argv,  &db_size, efile, &iterations, &split, &chunk, &verify, &scalar, &verbose)) {
+		ocrShutdown();
+		return NULL_GUID;
+	}
+
+	u64 nparamc = 9, rparamc = 7, pparamc = 2;
 	STREAM_TYPE * dataArray;
 	ocrGuid_t dataGuid, iterTemplateGuid, iterGuid, iterDone, resultsTemplateGuid, resultsGuid, 
 			  pipeExecTemplateGuid, nextIterTemplateGuid, pipelineTemplateGuid;
 	ocrEdtTemplateCreate(&iterTemplateGuid, &iterEdt, nparamc, 1);
-	ocrEdtTemplateCreate(&resultsTemplateGuid, &resultsEdt, 0, 2);
+	ocrEdtTemplateCreate(&resultsTemplateGuid, &resultsEdt, rparamc, 2);
 	ocrEdtTemplateCreate(&pipeExecTemplateGuid, &pipeExecEdt, nparamc, 1);
 	ocrEdtTemplateCreate(&nextIterTemplateGuid, &iterEdt, nparamc, 2);
 	ocrEdtTemplateCreate(&pipelineTemplateGuid, &pipelineEdt, nparamc + pparamc, 1);
 
-	u64 nparamv[4] = {1, pipeExecTemplateGuid, nextIterTemplateGuid, pipelineTemplateGuid};
+	u64 nparamv[9] = {1, db_size, iterations, split, chunk, scalar, pipeExecTemplateGuid, nextIterTemplateGuid, pipelineTemplateGuid};
+	u64 rparamv[7] = {db_size, iterations, split, chunk, verify, scalar, verbose};
 
 	// Formatting datablock
-	DBCREATE(&dataGuid,(void **) &dataArray, sizeof(STREAM_TYPE) * (3 * STREAM_ARRAY_SIZE + NTIMES), 0, NULL_GUID, NO_ALLOC);
-	for (i = 0; i < STREAM_ARRAY_SIZE; i++){
+	DBCREATE(&dataGuid,(void **) &dataArray, sizeof(STREAM_TYPE) * (3 * db_size + iterations), 0, NULL_GUID, NO_ALLOC);
+	for (i = 0; i < db_size; i++){
 		dataArray[i] = 1.0;
-		dataArray[STREAM_ARRAY_SIZE + i] = 2.0;
-		dataArray[2 * STREAM_ARRAY_SIZE + i] = 0.0;
+		dataArray[db_size + i] = 2.0;
+		dataArray[2 * db_size + i] = 0.0;
 	}
-	for (i = 3 * STREAM_ARRAY_SIZE; i < NTIMES; i++)
+	for (i = 3 * db_size; i < iterations; i++)
 		dataArray[i] = 0.0;
 
 	// Create iterator and results EDTs
 	ocrEdtCreate(&iterGuid, iterTemplateGuid, EDT_PARAM_DEF, nparamv, EDT_PARAM_DEF, NULL_GUID,
 				 EDT_PROP_FINISH, NULL_GUID, &iterDone);
-	ocrEdtCreate(&resultsGuid, resultsTemplateGuid, EDT_PARAM_DEF, NULL_GUID, EDT_PARAM_DEF, NULL_GUID,
+	ocrEdtCreate(&resultsGuid, resultsTemplateGuid, EDT_PARAM_DEF, rparamv, EDT_PARAM_DEF, NULL_GUID,
 				 EDT_PROP_NONE, NULL_GUID, NULL);
 
 	// Dependencies for iterator and results
 	ocrAddDependence(dataGuid, resultsGuid, 0, DB_MODE_RO);
 	ocrAddDependence(iterDone, resultsGuid, 1, DB_MODE_RO);
-	ocrAddDependence(dataGuid, iterGuid, 0, DB_MODE_EW);
+	ocrAddDependence(dataGuid, iterGuid, 0, DB_MODE_ITW);
 	// PRINTF("FINISHED MAIN\n");
 	return NULL_GUID;
 }
