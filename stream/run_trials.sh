3228f7892bd6dde6b85f500fa1fb5d942d356f9f
kp <kp70726F@gmail.com>
2014-04-27 01:56:16 -0700

    Added bash script to run several trials on various stream versions.

---
+++ stream/run_trials.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#make all
+
+min=100
+max=1000000
+i=100
+
+# serial stream runs
+for name in serial_stream serial_edt_breakdown_stream
+	do
+		for ((db=$min; db<=$max; db=db*10))
+			do 
+				echo ./$name -d $db -i $i -e $name.csv -r
+			done
+	done
+
+# parallel stream runs
+for name in parallel_stream parallel_db_breakdown_stream parallel_db_edt_breakdown_stream
+	do
+		for ((db=$min; db<=$max; db=db*10))
+			do
+				for ((p=1; p<=$db; p=p*10)) 
+					do
+						echo ./$name -d $db -i $i -e $name.csv -p $p -r
+					done
+			done
+	done
