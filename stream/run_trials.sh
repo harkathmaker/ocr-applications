84423b8a07c487e92d11ae079e84e638b7a5a4a5
kp <kp70726F@gmail.com>
2014-04-27 20:36:55 -0700

    Modified run_trials.sh to scale by factors of 2.

--- stream/run_trials.sh
+++ stream/run_trials.sh
@@ -2,29 +2,30 @@
 
 #make all
 
-min=100
-max=1000000
+min=1
+#2^24
+max=16777216
 i=100
 
 # serial stream runs
 for name in serial_stream serial_edt_breakdown_stream
 	do
-		for ((db=$min; db<=$max; db=db*10))
+		for ((db=$min; db<=$max; db=db*2))
 			do 
 				echo ./$name -d $db -i $i -e $name.csv -r
-				./$name -d $db -i $i -e $name.csv -r
+				#./$name -d $db -i $i -e $name.csv -r
 			done
 	done
 
 # parallel stream runs
 for name in parallel_stream parallel_db_breakdown_stream parallel_db_edt_breakdown_stream
 	do
-		for ((db=$min; db<=$max; db=db*10))
+		for ((db=$min; db<=$max; db=db*2))
 			do
-				for ((p=1; p<=$db; p=p*10)) 
+				for ((p=$min; p<=$db; p=p*2)) 
 					do
 						echo ./$name -d $db -i $i -e $name.csv -p $p -r
-						./$name -d $db -i $i -e $name.csv -p $p -r
+						#./$name -d $db -i $i -e $name.csv -p $p -r
 					done
 			done
 	done
