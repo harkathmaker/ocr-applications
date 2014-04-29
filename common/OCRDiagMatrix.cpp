06655bf519c60644073156355cc7e00df2dfe830
Matthew Unrath <harkathmaker@gmail.com>
2014-04-29 15:33:34 -0700

    Added diagonal matrices (need to test on an OCR system)

---
+++ common/OCRDiagMatrix.cpp
@@ -0,0 +1,31 @@
+#include "OCRDiagMatrix.h"
+
+OCRDiagMatrix::OCRDiagMatrix(void *_data, ocrGuid_t _datablock) {
+    ASSERT(_data != NULL);
+    data = _data;
+    datablock = _datablock;
+    rows = *((unsigned int*)_data);
+    columns = rows;
+    values = (double*)(_data+sizeof(unsigned int));
+}
+
+OCRDiagMatrix::OCRDiagMatrix(unsigned int sz) {
+    rows = columns = sz;
+    DBCREATE(&datablock, &data, sizeof(unsigned int) + sizeof(double)*sz,
+             0, NULL_GUID, NO_ALLOC);
+    *((unsigned int*)data) = sz;
+    values = (double*)(data+sizeof(unsigned int));
+}
+
+double OCRDiagMatrix::getElement(unsigned int row, unsigned int column) const {
+    if(row != column) {
+        return 0;
+    } else {
+        return values[row];
+    }
+}
+
+void OCRDiagMatrix::setElement(unsigned int row, unsigned int column, double val) {
+    assert(row == column);
+    values[row] = val;
+}
